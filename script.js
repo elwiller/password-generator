7f62e5aace20d55dbce16e891d53c2f1a1db2db8
elwiller <elwiller@gmail.com>
2020-06-28 17:48:53 -0500

    add options selection to page with additional css and js for slider

--- script.js
+++ script.js
@@ -10,5 +10,18 @@ function writePassword() {
 
 }
 
+const
+	range = document.getElementById('range'),
+	rangeV = document.getElementById('rangeV'),
+	setValue = ()=>{
+		const
+			newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
+			newPosition = 10 - (newValue * 0.2);
+		rangeV.innerHTML = `<span>${range.value}</span>`;
+		rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
+	};
+document.addEventListener("DOMContentLoaded", setValue);
+range.addEventListener('input', setValue);
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
