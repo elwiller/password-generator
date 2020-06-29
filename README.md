727043fe4ef653e6aa16da2c2406a95edf1a3f49
elwiller <elwiller@gmail.com>
2020-06-28 21:56:10 -0500

    update working code and add screenshots

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # password-generator
 03 JavaScript: Password Generator
+
+This is a nice Password Generator that will let you decide which characters you
+want included in your password. You may also select the length of your password
+from 8 to 128 characters in length.
+
+Please make sure you select at least one character set or your request will fail.
+
+![option selection screenshot](./pwdgenss1.png)
+![password generation screenshot](./pwdgenss2.png)
--- script.js
+++ script.js
@@ -3,13 +3,43 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+	var charSet1 = (document.getElementById("lower").checked);
+	var charSet2 = (document.getElementById("upper").checked);
+	var charSet3 = (document.getElementById("number").checked);
+	var charSet4 = (document.getElementById("special").checked);
+	if (charSet1 || charSet2 || charSet3 || charSet4) {
+		var password = generatePassword(charSet1, charSet2, charSet3, charSet4, range.value);
+		var optionsDisplay = document.querySelector("#options");
+		var passwordText = document.querySelector("#password");
+		var passwordDisplay = document.querySelector("#pwdHolder");
 
-  passwordText.value = password;
-
+		optionsDisplay.style.display = "none";
+		passwordText.value = password;
+		passwordDisplay.style.display = "block";
+	} else {
+			alert('Please select at least one character set\nfor password generation.');
+	}
 }
 
+// generate password
+function generatePassword(pwdLower, pwdUpper, pwdNumber, pwdSpecial, pwdLength) {
+	var lwrCse = "abcdefghijklmnopqrstuvwxyz";
+	var uprCse = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	var nmbrs = "1234567890";
+	var spcls = "!@#$%^&*()";
+	var charPool = "";
+	var actualPassword = "";
+	pwdLower ? charPool += lwrCse : null;
+	pwdUpper ? charPool += uprCse : null;
+	pwdNumber ? charPool += nmbrs : null;
+	pwdSpecial ? charPool += spcls : null;
+	for (i = 0; i < pwdLength; i++) {
+		actualPassword += charPool.charAt(Math.floor(Math.random() * charPool.length));
+	}
+	return actualPassword;
+}
+
+// "Price Range" slider by onyx1812 https://codepen.io/onyx1812/pen/GRJxmva
 const
 	range = document.getElementById('range'),
 	rangeV = document.getElementById('rangeV'),
